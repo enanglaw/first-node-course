0b4c01eda4d4212eebd853f512b60700c325d9de
Lawrence Bassey <awaselaurenz@yahoo.com>
2019-12-12 19:33:40 +0100

    Init_commit

---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+console.log('Client side javascript is loaded')
+
+/*fetch('http://puzzle.mead.io/puzzzle').then((response)=>
+    {
+        response.json().then((data)=>{
+            if(data.error){
+               console.log(data.error)
+            }
+            else
+            console.log(data);
+        })
+
+    })*/
+    
+    
+
+   
+
+    const formresult=document.querySelector('form')
+    formresult.addEventListener('submit',(e)=>{
+        e.preventDefault()
+
+
+        const x=document.getElementById('input_id').value
+        fetch('http://localhost:2121/weather?address='+ x ).then((response)=>
+    {
+        response.json().then((data)=>{
+            if(data.error){
+               console.log(data.error)
+            }
+            else
+            {
+            console.log(data.location);
+            console.log(data.forecast);
+            }
+        })
+
+	})
+   
+
+    })
---
+++ src/app.js
@@ -0,0 +1,64 @@
+
+const express=require('express')
+const path=require('path')
+const hbs =require('hbs')
+
+const app=express()
+const staticPathDir=path.join(__dirname,'../public')
+const staticPathDirForStyles=path.join(__dirname,'bootstrap')
+const viewPathDir=path.join(__dirname,'../templates/views')
+
+
+const partialPath=path.join(__dirname,'../templates/partials')
+
+
+
+
+
+app.use(express.static(staticPathDir))
+//app.use(express.static(staticPathDirForStyles))
+
+
+
+app.set('view engine','hbs')
+app.set('views',viewPathDir)
+hbs.registerPartials(partialPath)
+
+
+app.get('/product',(req, res)=>{
+ 
+   if(!req.query.search){
+   return res.render('404',{title:'404: searched item not found'});
+   }
+   else
+   {
+
+    res.render('product', {products:listOfProduct(),name:'Enang Lawrence'});
+   }
+
+});
+products=[{name:'Iphone', rating:4.7},{name:'tecno', rating:4.3},{name:'Sunsung', rating:4.7}];
+
+const listOfProduct=(()=>
+{
+ 
+products.forEach(product => {
+  console.log( product.name, product.rating);
+});
+return products;
+});
+listOfProduct();
+app.get('/weather',(req, res)=>{
+  res.render('weather',{
+    weather:'weather page',name:'Enang Lawrence'
+
+  });
+});
+app.get('*',(request, response)=>{
+  response.render('404',{
+      title:'resources not found',name:'Enang Lawrence'
+  })
+  })
+app.listen('2121',()=> {
+    console.log('running and Listening live on local port 2121');
+});
